4bd32e31dd267a3f626755369036e4efc1768466
SAYAN02-DEV <sayan2003.dev@gmail.com>
2026-05-15 16:13:42 +0530

    Decision: changed from grom to sqlc+pgx

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,16 +1,14 @@
 package db
 
 import (
+	"context"
 	"log"
 
 	"github.com/SAYAN02-DEV/iron-bench/internal/config"
-	"github.com/SAYAN02-DEV/iron-bench/models"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-var DB *gorm.DB
+var Pool *pgxpool.Pool
 
 func Connect(cfg *config.Config) {
 	connStr := cfg.DatabaseURL
@@ -18,29 +16,22 @@ func Connect(cfg *config.Config) {
 		log.Fatal("DATABASE_URL is not set")
 	}
 
-	var err error
-	DB, err = gorm.Open(postgres.Open(connStr), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Error),
-	})
+	pool, err := pgxpool.New(context.Background(), connStr)
 	if err != nil {
 		log.Fatal("failed to connect:", err)
 	}
+	if err := pool.Ping(context.Background()); err != nil {
+		log.Fatal("failed to ping database:", err)
+	}
 
-	DB.AutoMigrate(&models.User{})
+	Pool = pool
 
 	log.Println("Connected to database")
 }
 
 func Close() {
-	if DB == nil {
+	if Pool == nil {
 		return
 	}
-	sqlDB, err := DB.DB()
-	if err != nil {
-		log.Println("failed to get sql DB:", err)
-		return
-	}
-	if err := sqlDB.Close(); err != nil {
-		log.Println("failed to close DB:", err)
-	}
+	Pool.Close()
 }
